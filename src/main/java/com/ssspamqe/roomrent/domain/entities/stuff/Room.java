8d400b107684a75e3d202e691246ba2edabd7798
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-10-26 20:42:45 +0300

    Add description column to rooms table

--- src/main/java/com/ssspamqe/roomrent/domain/entities/stuff/Room.java
+++ src/main/java/com/ssspamqe/roomrent/domain/entities/stuff/Room.java
@@ -33,6 +33,9 @@ public class Room {
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "description")
+    private String description;
+
     @ManyToOne
     @JoinColumn(name = "seller_id", nullable = false)
     private Seller owner;
