9e540e5d10296d80411d81233647e03565ffb2c9
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-11-04 19:50:16 +0300

    Add Geometry.Point mapping

--- src/main/java/com/ssspamqe/roomrent/mappers/RoomMapper.java
+++ src/main/java/com/ssspamqe/roomrent/mappers/RoomMapper.java
@@ -4,20 +4,26 @@ import com.ssspamqe.roomrent.configuration.mapstruct.MapStructConfig;
 import com.ssspamqe.roomrent.controllers.room.request.RoomDTO;
 import com.ssspamqe.roomrent.domain.entities.stuff.Room;
 import com.ssspamqe.roomrent.domain.entities.users.Seller;
+import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapStructConfig.class)
-public interface RoomMapper {
+@RequiredArgsConstructor
+public abstract class RoomMapper {
 
-    Room toEntity(RoomDTO roomDTO, Seller owner);
+    private final GeometryMapper geometryMapper;
+
+    @Mapping(target = "position", expression = "java(geometryMapper.toPoint(roomDTO.position()))")
+    @Mapping(target = "owner", source = "owner")
+    abstract Room toEntity(RoomDTO roomDTO, Seller owner);
 
     @Mapping(target = "rents", ignore = true)
     @Mapping(target = "owner", ignore = true)
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "deleted", ignore = true)
     @Mapping(target = "announcements", ignore = true)
-    void update(@MappingTarget Room room, RoomDTO roomDTO);
-
+    @Mapping(target = "position", expression = "java(geometryMapper.toPoint(roomDTO.position()))")
+    abstract void update(@MappingTarget Room room, RoomDTO roomDTO);
 }
